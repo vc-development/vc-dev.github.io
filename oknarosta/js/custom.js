984e9e95a1a47f6107c548e7642c90d9ac7971a6
vc-development <slivka@ecookna.ru>
2017-09-13 14:55:03 +0300

    Add Barba.js and cleanup

--- oknarosta/js/custom.js
+++ oknarosta/js/custom.js
@@ -1,4 +1,44 @@
 jQuery(document).ready(function($) {
+    // Barba.js
+    // Barba.Pjax.start();
+	//
+    // var FadeTransition = Barba.BaseTransition.extend({
+    //     start: function() {
+    //         Promise
+    //             .all([this.newContainerLoading, this.fadeOut()])
+    //             .then(this.fadeIn.bind(this));
+    //     },
+	//
+    //     fadeOut: function() {
+    //         return $(this.oldContainer).animate({
+    //             opacity: 0
+    //         }).promise();
+    //     },
+	//
+    //     fadeIn: function() {
+    //         var _this = this;
+    //         var $el = $(this.newContainer);
+	//
+    //         $(this.oldContainer).hide();
+	//
+    //         $el.css({
+    //             visibility: 'visible',
+    //             opacity: 0
+    //         });
+	//
+    //         $el.animate({
+    //             opacity: 1
+    //         }, 400, function() {
+    //             _this.done();
+    //         });
+    //     }
+    // });
+	//
+    // Barba.Pjax.getTransition = function() {
+    //     return FadeTransition;
+    // };
+
+    // Input Mask
     $(".phone-mask").inputmask({
         mask: "+7 (999) 999-99-99"
     });
