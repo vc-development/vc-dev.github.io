222929280d957b041cd8d33bb04380691703853c
vc-development <slivka@ecookna.ru>
2017-09-22 12:07:09 +0300

    Up

--- rusokon/js/custom.js
+++ rusokon/js/custom.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
     $("#rs-carousel").slick({
         accessibility: false,
         arrows: false,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 3000,
         rows: 1,
         slidesToShow: 3,
