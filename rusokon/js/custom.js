e036e8c381485ffc994b6e6b9d823b09595a259f
vc-development <slivka@ecookna.ru>
2017-09-22 11:00:53 +0300

    Up

--- rusokon/js/custom.js
+++ rusokon/js/custom.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
         speed: 500,
         fade: true,
         cssEase: 'linear',
+		waitForAnimate: false,
         prevArrow: '<a href="#" class="uk-position-center-left uk-position-medium uk-position-z-index" uk-icon="icon: chevron-left; ratio: 3"></a>',
         nextArrow: '<a href="#" class="uk-position-center-right uk-position-medium uk-position-z-index" uk-icon="icon: chevron-right; ratio: 3"></a>'
     });
@@ -19,7 +20,10 @@ $(document).ready(function() {
         autoplaySpeed: 3000,
         rows: 1,
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+		dots: true,
+		dotsClass: 'uk-dotnav',
+		waitForAnimate: false
     });
 
 });
