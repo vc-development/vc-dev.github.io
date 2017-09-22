3a9f9d4e05b5b13d34b30ef6857e928335eea709
vc-development <slivka@ecookna.ru>
2017-09-22 09:53:40 +0300

    Add lightbox to carousel

--- rusokon/js/custom.js
+++ rusokon/js/custom.js
@@ -1,9 +1,19 @@
 jQuery(document).ready(function($) {
-	$(".rs-carousel").slick({
-		accessibility: false,
-		autoplay: true,
-		autoplaySpeed: 3000,
-		rows: 1,
-		slidesToShow: 3
-	});
+
+    // Slick Carousel
+    $(".rs-carousel").slick({
+        accessibility: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        rows: 1,
+        slidesToShow: 3,
+		slidesToScroll: 1
+    });
+    $("#next").click(function() {
+        $(".your-class").slick("slickNext");
+    });
+    $("#prev").click(function() {
+        $(".your-class").slick("slickPrev");
+    });
+
 });
