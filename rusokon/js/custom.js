a34f20401b43df4af326eb4d12834a569216c5c9
vc-development <slivka@ecookna.ru>
2017-09-22 10:00:49 +0300

    Add Fade

--- rusokon/js/custom.js
+++ rusokon/js/custom.js
@@ -1,9 +1,14 @@
 jQuery(document).ready(function($) {
 
-	$("#rs-slider").slick({
-		accessibility: false,
-		autoplay: true
-	});
+    $("#rs-slider").slick({
+        accessibility: false,
+        autoplay: true,
+		autoplaySpeed: 5000,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 
     // Slick Carousel
     $("#rs-carousel").slick({
@@ -12,7 +17,7 @@ jQuery(document).ready(function($) {
         autoplaySpeed: 3000,
         rows: 1,
         slidesToShow: 3,
-		slidesToScroll: 1
+        slidesToScroll: 1
     });
     // $("#next").click(function() {
     //     $(".your-class").slick("slickNext");
