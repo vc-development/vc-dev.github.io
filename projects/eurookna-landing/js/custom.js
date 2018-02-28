18a32ba1e866393092ef6aa6de6ce2fac779406d
vc-development <30495232+vc-development@users.noreply.github.com>
2018-02-28 14:54:51 +0300

    Up

--- projects/eurookna-landing/js/custom.js
+++ projects/eurookna-landing/js/custom.js
@@ -1,5 +1,17 @@
 jQuery(document).ready(function($) {
+
+	// InputMask
     $(".phone-mask").inputmask({
         mask: "+7 (999) 999-99-99"
     });
+
+	// Animation Gift
+	setInterval(function() {
+	    var animationName = 'animated tada';
+	    var animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+	    $('.gift').addClass(animationName).one(animationend, function() {
+			$(this).removeClass(animationName);
+		});
+	}, 3000);
+
 });
