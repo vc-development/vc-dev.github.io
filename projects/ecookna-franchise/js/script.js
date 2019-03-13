9cba89dc9206862a7321de47a85b2c6b1f68e82a
vc-development <slivka@ecookna.ru>
2019-03-13 12:32:06 +0300

    Up

--- projects/ecookna-franchise/js/script.js
+++ projects/ecookna-franchise/js/script.js
@@ -1,15 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
-	//--- TypeIt
-	// Rewiew
-	// var reviewTypeIt = new TypeIt('.js-review-typeit', {
-	// 	strings: '',
-	// 	speed: 5,
-	// 	lifeLike: false,
-	// 	cursor: false,
-	// 	waitUntilVisible: true
-	// }).go();
 
-	// Price
+	// countUp.js
+    var countUpOptions = {useEasing: true, useGrouping: true, separator: ' ', decimal: ''}
+    var countUp = new CountUp('js-countup', 0, 60874, 0, 3, countUpOptions);
+    var salon1 = new CountUp('js-countup-salon1', 0, 5, 0, 3, countUpOptions);
+    var salon2 = new CountUp('js-countup-salon2', 0, 20, 0, 3, countUpOptions);
+    var salon3 = new CountUp('js-countup-salon3', 0, 54, 0, 3, countUpOptions);
+    var salon4 = new CountUp('js-countup-salon4', 0, 68, 0, 3, countUpOptions);
+
+    UIkit.util.on('.js-countup', 'inview', function() {
+        countUp.start();
+        salon1.start();
+        salon2.start();
+        salon3.start();
+        salon4.start();
+    });
+
+	// TypeIt: Price
 	var priceTypeIt = new TypeIt('.js-price-typeit', {
 		strings: '',
 		speed: 30,
@@ -18,7 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		waitUntilVisible: true
 	}).go();
 
-
 	//--- Inputmask
 	Inputmask({
 		placeholder: '_',
@@ -26,10 +32,4 @@ document.addEventListener('DOMContentLoaded', function () {
 		mask: '+7 (999) 999-99-99'
 	}).mask(document.querySelectorAll('input[type="tel"]'));
 
-
-	//--- Superplaceholder
-	superplaceholder({
-		el: document.querySelector('input[type="email"]'),
-		sentences: ['Укажите Ваш e-mail', 'Например: name@yandex.ru', ' ']
-	});
 });
