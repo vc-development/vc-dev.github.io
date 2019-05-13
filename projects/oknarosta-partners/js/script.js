564ea9b99a2f1ea89636ac5b1cabbbb166bd61b8
vc-development <slivka@ecookna.ru>
2019-05-13 16:43:25 +0300

    Up

--- projects/oknarosta-partners/js/script.js
+++ projects/oknarosta-partners/js/script.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-	// Code...
+	new Inputmask({
+        placeholder: '_',
+        mask: '+7 (999) 999-99-99'
+    }).mask(document.querySelectorAll('input[type="tel"]'))
 })
