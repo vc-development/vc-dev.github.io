8d535e7836311cb44daa6dc65d55c24527734738
vc-development <slivka@ecookna.ru>
2019-05-22 15:22:21 +0300

    Up

--- projects/oknarosta-partners/js/script.js
+++ projects/oknarosta-partners/js/script.js
@@ -3,4 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
 	// Inputmask
 	new Inputmask('+7 (999) 999-99-99').mask(document.querySelectorAll('input[type="tel"]'));
 
+	// Yandex Map
+	ymaps.ready(init);
+
+	function init() {
+		var myMap = new ymaps.Map('ymap', {
+			center: [55.610828, 37.723997],
+			zoom: 15,
+			controls: ['zoomControl']
+		});
+
+		myMap.behaviors.disable('scrollZoom');
+
+		var myPlacemark = new ymaps.Placemark(
+			[55.610828, 37.723997], {
+				balloonContent: '127006, г. Москва, ул. Генерала Белова, 35',
+				hintContent: '127006, г. Москва, ул. Генерала Белова, 35',
+				iconCaption: '«Окна Роста»'
+			}, {
+				preset: 'islands#greenDotIconWithCaption',
+				iconColor: '#cc0000'
+			}
+		)
+		myMap.geoObjects.add(myPlacemark);
+	}
 });
